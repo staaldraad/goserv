199683d4cfa0cd12766f2c2764bf76d46d7b2b25
Etienne Stalmans <estalmans@gmail.com>
2020-05-26 10:14:02 +0200

    Stronger generated certificates. Add option to secure connection with mTLS

--- goserv.go
+++ goserv.go
@@ -3,12 +3,15 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -25,7 +28,7 @@ func genCert() {
 		}
 	}
 	fmt.Println("[*] No certificate files found in directory. Generating new...")
-	s, _ := rand.Prime(rand.Reader, 1024)
+	s, _ := rand.Prime(rand.Reader, 2048)
 	ca := &x509.Certificate{
 		SerialNumber: s,
 		Subject: pkix.Name{
@@ -37,14 +40,14 @@ func genCert() {
 		NotAfter:              time.Now().AddDate(10, 0, 0),
 		SubjectKeyId:          []byte{1, 2, 3, 4, 6},
 		BasicConstraintsValid: true,
-		IsCA:        true,
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 	}
 
-	priv, _ := rsa.GenerateKey(rand.Reader, 1024)
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
 	pub := &priv.PublicKey
-	ca_b, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
+	cab, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
 	if err != nil {
 		fmt.Println("create ca failed", err)
 	}
@@ -71,24 +74,24 @@ func genCert() {
 	}
 
 	kpemfile.Close()
-	pem.Encode(cpemfile, &pem.Block{Type: "CERTIFICATE", Bytes: ca_b})
+	pem.Encode(cpemfile, &pem.Block{Type: "CERTIFICATE", Bytes: cab})
 	cpemfile.Close()
 
 	fmt.Println("[*] Certificate files generated")
 }
 
 func redirRequest(w http.ResponseWriter, req *http.Request) {
-    dst := req.URL.Query().Get("r")
-    fmt.Printf("[%s][Accepted -  %s][From: %s][Redirect: %s]\n", time.Now(),req.URL, req.RemoteAddr,dst)
-    http.Redirect(w,req,dst,302)
+	dst := req.URL.Query().Get("r")
+	fmt.Printf("[%s][Accepted -  %s][From: %s][Redirect: %s]\n", time.Now(), req.URL, req.RemoteAddr, dst)
+	http.Redirect(w, req, dst, 302)
 }
 
 func redir2Request(w http.ResponseWriter, req *http.Request) {
-    dst := req.URL.Query().Get("r")
-    fmt.Printf("[%s][Accepted -  %s][From: %s][Redirect: %s]\n", time.Now(),req.URL, req.RemoteAddr,dst)
-    //http.Redirect(w,req,dst,302)
-    w.Header().Add("Location", "`curl `")
-    w.WriteHeader(302)
+	dst := req.URL.Query().Get("r")
+	fmt.Printf("[%s][Accepted -  %s][From: %s][Redirect: %s]\n", time.Now(), req.URL, req.RemoteAddr, dst)
+	//http.Redirect(w,req,dst,302)
+	w.Header().Add("Location", "`curl `")
+	w.WriteHeader(302)
 
 	fmt.Fprintf(w, "ok")
 	return
@@ -96,15 +99,15 @@ func redir2Request(w http.ResponseWriter, req *http.Request) {
 }
 
 func logRequest(w http.ResponseWriter, req *http.Request) {
-    t := time.Now().Format(time.UnixDate)
-    fmt.Printf("[%s][Accepted -  %s][From: %s]\n", t, req.URL, req.RemoteAddr)
+	t := time.Now().Format(time.UnixDate)
+	fmt.Printf("[%s][Accepted -  %s][From: %s]\n", t, req.URL, req.RemoteAddr)
 
 	if hostHddrs == true {
-        fmt.Println("---Headers---")
+		fmt.Println("---Headers---")
 		for k, v := range req.Header {
 			fmt.Printf("%s : %s\n", k, v)
 		}
-        fmt.Println("----------")
+		fmt.Println("----------")
 	}
 	if req.Method == "POST" {
 		req.ParseForm()
@@ -120,7 +123,7 @@ func logRequest(w http.ResponseWriter, req *http.Request) {
 			if err != nil && err != io.EOF {
 				fmt.Println(buf, err)
 			} else {
-			    fmt.Println("-----POST-----")
+				fmt.Println("-----POST-----")
 				fmt.Printf("\n%s\n", buf)
 			}
 		}
@@ -129,11 +132,11 @@ func logRequest(w http.ResponseWriter, req *http.Request) {
 	}
 	if _, err := os.Stat(fmt.Sprintf("%s/%s", hostDir, req.URL)); err != nil {
 		fmt.Printf("[%s][404] %s\n", req.RemoteAddr, req.URL)
-        w.WriteHeader(200)
-        fmt.Fprintf(w,"")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "")
 	} else if req.URL.Path == "/key.pem" || req.URL.Path == "/cert.pem" {
 		fmt.Printf("[%s][403] %s\n", req.RemoteAddr, req.URL)
-        w.WriteHeader(403)
+		w.WriteHeader(403)
 		fmt.Fprintf(w, "Nop, sorry...")
 	} else {
 		fmt.Printf("[%s][200] %s\n", req.RemoteAddr, req.URL)
@@ -149,30 +152,30 @@ func logRequest(w http.ResponseWriter, req *http.Request) {
 }
 
 func uploadRequest(w http.ResponseWriter, req *http.Request) {
-    t := time.Now().Format(time.UnixDate)
-    fmt.Printf("[%s][Accepted -  %s][From: %s]\n", t, req.URL, req.RemoteAddr)
+	t := time.Now().Format(time.UnixDate)
+	fmt.Printf("[%s][Accepted -  %s][From: %s]\n", t, req.URL, req.RemoteAddr)
 
 	if req.Method == "GET" {
-        w.WriteHeader(200)
+		w.WriteHeader(200)
 		fmt.Fprintf(w, "ok")
 		return
-    } else {
-        file, _, err := req.FormFile("data")
-        if err != nil {
-            fmt.Println(err)
-            w.WriteHeader(500)
-            fmt.Fprintf(w, "ok")
-            return
-        }
-        defer file.Close()
+	} else {
+		file, _, err := req.FormFile("data")
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "ok")
+			return
+		}
+		defer file.Close()
 
-        // copy example
-        f, err := os.OpenFile(fmt.Sprintf("./upload_%d",time.Now().Unix()), os.O_WRONLY|os.O_CREATE, 0666)
-        defer f.Close()
-        io.Copy(f, file)
-   }
-        w.WriteHeader(200)
-		fmt.Fprintf(w, "ok")
+		// copy example
+		f, err := os.OpenFile(fmt.Sprintf("./upload_%d", time.Now().Unix()), os.O_WRONLY|os.O_CREATE, 0666)
+		defer f.Close()
+		io.Copy(f, file)
+	}
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "ok")
 }
 
 func main() {
@@ -180,6 +183,7 @@ func main() {
 	dirPtr := flag.String("d", "./", "The directory to share")
 	tlsPtr := flag.Bool("s", false, "Share using HTTPS (default=false)")
 	hdrsPtr := flag.Bool("headers", false, "Print incomming headers")
+	mTLSPtr := flag.String("m", "", "client certificate to use for mtls")
 	flag.Parse()
 
 	var err error
@@ -204,7 +208,26 @@ func main() {
 
 	if *tlsPtr == true {
 		genCert()
-		err = http.ListenAndServeTLS(fmt.Sprintf(":%d", *portPtr), "cert.pem", "key.pem", nil)
+
+		tlsConfig := &tls.Config{InsecureSkipVerify: true}
+		if *mTLSPtr != "" {
+			caCert, err := ioutil.ReadFile(*mTLSPtr)
+			if err != nil {
+				log.Fatal(err)
+			}
+			caCertPool := x509.NewCertPool()
+			caCertPool.AppendCertsFromPEM(caCert)
+			tlsConfig.ClientCAs = caCertPool
+			tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+		}
+
+		tlsConfig.BuildNameToCertificate()
+		server := &http.Server{
+			Addr:      fmt.Sprintf(":%d", *portPtr),
+			TLSConfig: tlsConfig,
+		}
+
+		err = server.ListenAndServeTLS("cert.pem", "key.pem")
 	} else {
 		err = http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), nil)
 	}
